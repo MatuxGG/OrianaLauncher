e018f17be9e2edaafb30570c7d591edb3ae581b9
MatuxGG <mattthedark.extinction@gmail.com>
2021-05-25 20:03:34 +0200

    follow up for last update

--- OrianaLauncher/Class/Applist.cs
+++ OrianaLauncher/Class/Applist.cs
@@ -21,7 +21,7 @@ namespace OrianaLauncher.Class
         public async Task load()
         {
             this.apps.Add(new App("Origin", "Lunastellia", "Challenger-Among-Us", new string[] { "C" }, new string[] { "B", "L" }, "Among Us.exe", false));
-            this.apps.Add(new App("Challenger", "MatuxGG", "Challenger-Among-Us", new string[] { }, new string[] { "B", "L", "C", "A" }, "Among Us.exe", true));
+            this.apps.Add(new App("Challenger", "Lunastellia", "Challenger-Among-Us", new string[] { }, new string[] { "B", "L", "C", "A" }, "Among Us.exe", true));
             this.apps.Add(new App("Challenger Lite", "Lunastellia", "Challenger-Among-Us", new string[] { "L" }, new string[] { "B", "C", "A" }, "Among Us.exe", false));
             this.apps.Add(new App("Challenger Beta", "Lunastellia", "Challenger-Among-Us", new string[] { "B" }, new string[] { "L", "C", "A" }, "Among Us.exe", false));
 
